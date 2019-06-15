f3237840467abb59125cbad3cae5ad2310f066b8
org0000h <yazhou05@gmail.com>
2019-06-15 16:39:20 +0800

    add multiple servers

--- src/config_server.js
+++ src/config_server.js
@@ -1,8 +1,15 @@
 config_server = {
-    port :3000,
     http_enable :true,
-    tls_enable :true,
-    websocket_enable :true,
+    http_port :80,
+
+    https_enable :true,
+    https_port :443,
+
+    websocket :true,
+    websocket_port :3000,
+
+    websocket_ssl :true,
+    websocket_ssl_port :3001,
     db_enable :false,
     maxSockets :40000
 }
--- src/server.js
+++ src/server.js
@@ -1,80 +1,103 @@
 #!/usr/bin/env node
 const server_config = require('./config_server');
 const app = require('./app');
+const http = require('http');
+const https = require('https');
+http.globalAgent.maxSockets = server_config.maxSockets;
 
-let server = null;
-//  
-if(server_config.tls_enable){
-    const https = require('https');
-    https.globalAgent.maxSockets = server_config.maxSockets;
+let servers = {};
+
+//http server
+if(server_config.http_enable){
+    servers.http_server = http.createServer(app.callback());
+}
+
+//https server
+if(server_config.https_enable){
     const fs = require('fs');
     //tls key and crt
     let privateKey  = fs.readFileSync(__dirname + '/../ecc_cert/ecc.key', 'utf8');
     let certificate = fs.readFileSync(__dirname + '/../ecc_cert/ecc.crt', 'utf8');
     let credentials = {key: privateKey, cert: certificate};
-    server = https.createServer(credentials,app.callback());
-}else{
-    const http = require('http');
-    http.globalAgent.maxSockets = server_config.maxSockets;
-    server = http.createServer(app.callback());
+    servers.https_server = https.createServer(credentials,app.callback());
 }
-
-if(server_config.websocket_enbale){
+// websocket 
+if(server_config.websocket){
     const socket_io = require('socket.io');
     const webSocketService = require('./webSocketService');
-    let io = socket_io(server);
+
+    servers.websocket_server = http.createServer();
+
+    let io = socket_io(servers.websocket_server);
     webSocketService(io);
+    if(server_config.websocket_ssl){
+        servers.websocket_ssl_server = https.createServer();
+        let io = socket_io(servers.websocket_ssl_server);
+        webSocketService(io);
+    }
 }
 
- /**
-   * Event listener for HTTP server "error" event.
-   */
-  function generateOnError(port){
-    return  (error) => {
-      if (error.syscall !== 'listen') {
-        throw error;
-      }
-    
-      var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-    
-      // handle specific listen errors with friendly messages
-      switch (error.code) {
-        case 'EACCES':
-          console.error(bind + ' requires elevated privileges');
-          process.exit(1);
-          break;
-        case 'EADDRINUSE':
-          console.error(bind + ' is already in use');
-          process.exit(1);
-          break;
-        default:
-          throw error;
-      }
+startServer(servers);
+
+// function define
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function generateOnError(port){
+return  (error) => {
+    if (error.syscall !== 'listen') {
+    throw error;
     }
-  }
 
-let port = config_server.port;
+    var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
 
-let onError = generateOnError(port);
-server.on('error', onError);
-server.on('listening',()=>{
-  console.log('listening on port:' + port);
-});
-
-if (server_config.db_enable) {
-  const db = require('../persistence/db');
-  require("../persistence/loadModels");
-  db.sync()
-  .then(()=>{
-      console.log("\r\n Data base init done");
-      server.listen(port);
-  })
-  .catch((e) => { 
-      console.log(`failed:${e}`); process.exit(0); 
-  });
-
-}else{
-  server.listen(port);
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+    case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+    case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+    default:
+        throw error;
+    }
+}
+}
+function printListeningServer(server, port){
+    return ()=>{
+        console.log(`${server} 'is listening on port: + ${port}`);
+      }
+}
+function startServer(servers){
+    ports_of_servers = {
+        http_server:server_config.http_port,
+        https_server:server_config.https_port,
+        websocket_server:server_config.websocket_port,
+        websocket_ssl_server:server_config.websocket_ssl_port,
+    }
+    for(server in servers){
+        let onError = generateOnError(ports_of_servers[server]);
+        servers[server].on('error', onError);
+        servers[server].on('listening',printListeningServer(server, ports_of_servers[server]));
+        if (server_config.db_enable) {
+            const db = require('../persistence/db');
+            require("../persistence/loadModels");
+            db.sync()
+            .then(()=>{
+                console.log("\r\n Database sync done");
+                servers[server].listen(ports_of_servers[server]);
+            })
+            .catch((e) => { 
+                console.log(`failed:${e}`); process.exit(0); 
+            });
+          }else{
+            servers[server].listen(ports_of_servers[server]);
+          }
+    }
+    
 }
