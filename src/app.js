5968680636a10e8c4d19e57bd817544d967215d9
org0000h <yazhou05@gmail.com>
2019-06-17 16:03:08 +0800

    airbnb eslint pass

--- src/app.js
+++ src/app.js
@@ -1,34 +1,32 @@
-"use strict";
-const Koa =             require('koa');
-const koa_morgan =      require('koa-morgan');
-const koa_helmet =      require('koa-helmet');
-const koa_body =        require('koa-body');
-const cors =            require('@koa/cors');
-const static_cache =    require('koa-static-cache');
-const serve =           require('koa-static');
-const koa_json_mask =   require('koa-json-mask');
-const koa_compose =     require('koa-compose');
+const Koa = require('koa');
+const morgan = require('koa-morgan');
+const helmet = require('koa-helmet');
+const body = require('koa-body');
+const cors = require('@koa/cors');
+const staticCache = require('koa-static-cache');
+const jsonMask = require('koa-json-mask');
+const koaCompose = require('koa-compose');
 
 const app = new Koa();
 
 // third patry middleware
 // HTTP requests logger
 if (process.env.NODE_ENV !== 'test') {
-  app.use(koa_morgan('[:date[clf]] ":method :url HTTP/:http-version" :status - :response-time ms'));
+  app.use(morgan('[:date[clf]] ":method :url HTTP/:http-version" :status - :response-time ms'));
 }
 
-// my middleware
-const router_user = require('./routes/user');
+//  my middleware
+const routerUser = require('./routes/user');
 
-//load middleware
-const all = koa_compose([
-  koa_helmet(),// HTTP header security
+//  load middleware
+const all = koaCompose([
+  helmet(), // HTTP header security
   cors(),
-  static_cache('./satic_files', {maxAge: 60 * 60}),
-  koa_body({jsonLimit: '1kb'}),
-  koa_json_mask(),// Allow user to restrict the keys returned
+  staticCache('./satic_files', { maxAge: 60 * 60 }),
+  body({ jsonLimit: '1kb' }),
+  jsonMask(), // Allow user to restrict the keys returned
 
-  router_user.routes(),
+  routerUser.routes(),
 ]);
 app.use(all);
 
--- src/server.js
+++ src/server.js
@@ -1,113 +1,111 @@
 #!/usr/bin/env node
-const server_config = require('./config_server');
-const app = require('./app');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-const socket_io = require('socket.io');
+const socketIO = require('socket.io');
+const app = require('./app');
+const serverConfig = require('./config_server');
 const webSocketService = require('./webSocketService');
 const db = require('./database/db');
-require("./database/loadModels");
-http.globalAgent.maxSockets = server_config.maxSockets;
+require('./database/loadModels');
 
-let servers = {};
+http.globalAgent.maxSockets = serverConfig.maxSockets;
 
-//create http server
-if(server_config.http_enable){
-    servers.http_server = http.createServer(app.callback());
+const serversRunning = {};
+
+function loadTlsInfo(pathKey, pathCrt) {
+  const privateKey = fs.readFileSync(pathKey, 'utf8');
+  const certificate = fs.readFileSync(pathCrt, 'utf8');
+  return { key: privateKey, cert: certificate };
 }
 
-//create https server
-if(server_config.https_enable){
-    let credentials = loadTlsInfo(
-        __dirname + '/../ecc_cert/ecc.key', 
-        __dirname + '/../ecc_cert/ecc.crt');
-    servers.https_server = https.createServer(credentials,app.callback());
-}
-//create websocket server 
-if(server_config.websocket){
-    servers.websocket_server = http.createServer();
-    let io = socket_io(servers.websocket_server);
-    webSocketService(io);
-}
-//create websocket server over tls        
-if(server_config.websocket_ssl){
-    let credentials = loadTlsInfo(
-        __dirname + '/../ecc_cert/ecc.key', 
-        __dirname + '/../ecc_cert/ecc.crt');
-    servers.websocket_ssl_server = https.createServer(credentials);
-    let io = socket_io(servers.websocket_ssl_server);
-    webSocketService(io);
-}
-startServer(servers);
+function generateOnError(port) {
+  return (error) => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
 
-// function define
-/**
- * Event listener for HTTP server "error" event.
- */
-function generateOnError(port){
-    return  (error) => {
-        if (error.syscall !== 'listen') {
+    const bind = typeof port === 'string'
+      ? `Pipe ${port}`
+      : `Port ${port}`;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        process.stderr.write(`${bind} requires elevated privileges`);
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        process.stderr.write(`${bind} is already in use`);
+        process.exit(1);
+        break;
+      default:
         throw error;
-        }
-
-        var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-        case 'EACCES':
-            process.stderr.write(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            process.stderr.write(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-        }
     }
-}
-function printListeningServer(server, port){
-    return ()=>{
-        process.stdout.write(`${server} 'is listening on port: + ${port}\r\n`);
-      }
-}
-function startServer(servers){
-    if (server_config.db_enable) {
-        db.sync()
-        .then(()=>{
-            process.stdout.write("\r\nDatabase sync done");
-            servers_listen(servers);
-        })
-        .catch((e) => { 
-            process.stderr.write(`failed:${e}`); process.exit(0); 
-        });
-      }else{
-        servers_listen(servers);
-    }
-
-    
+  };
 }
 
-function loadTlsInfo(path_key, path_crt){
-    let privateKey  = fs.readFileSync(path_key, 'utf8');
-    let certificate = fs.readFileSync(path_crt, 'utf8');
-    return  {key: privateKey, cert: certificate};
+function printListeningServer(server, port) {
+  return () => {
+    process.stdout.write(`${server} 'is listening on port: + ${port}\r\n`);
+  };
 }
-function servers_listen(servers){
-    let ports_of_servers = {
-        http_server:server_config.http_port,
-        https_server:server_config.https_port,
-        websocket_server:server_config.websocket_port,
-        websocket_ssl_server:server_config.websocket_ssl_port,
-    }
-    for(let server in servers){
-        let onError = generateOnError(ports_of_servers[server]);
-        servers[server].on('error', onError);
-        servers[server].on('listening',printListeningServer(server, ports_of_servers[server]));
-        servers[server].listen(ports_of_servers[server]);
-    }
-} 
+function serversListen(servers) {
+  const portsOfServers = {
+    http_server: serverConfig.http_port,
+    https_server: serverConfig.https_port,
+    websocket_server: serverConfig.websocket_port,
+    websocket_ssl_server: serverConfig.websocket_ssl_port,
+  };
+
+  Object.keys(servers).forEach((server) => {
+    const onError = generateOnError(portsOfServers[server]);
+    servers[server].on('error', onError);
+    servers[server].on('listening', printListeningServer(server, portsOfServers[server]));
+    servers[server].listen(portsOfServers[server]);
+  });
+}
+function startServer(servers) {
+  if (serverConfig.db_enable) {
+    db.sync()
+      .then(() => {
+        process.stdout.write('\r\nDatabase sync done');
+        serversListen(servers);
+      })
+      .catch((e) => {
+        process.stderr.write(`failed:${e}`); process.exit(0);
+      });
+  } else {
+    serversListen(servers);
+  }
+}
+//  create http server
+if (serverConfig.http_enable) {
+  serversRunning.http_server = http.createServer(app.callback());
+}
+
+//  create https server
+if (serverConfig.https_enable) {
+  const credentials = loadTlsInfo(
+    `${__dirname}/../ecc_cert/ecc.key`,
+    `${__dirname}/../ecc_cert/ecc.crt`,
+  );
+  serversRunning.https_server = https.createServer(credentials, app.callback());
+}
+// create websocket server
+if (serverConfig.websocket) {
+  serversRunning.websocket_server = http.createServer();
+  const io = socketIO(serversRunning.websocket_server);
+  webSocketService(io);
+}
+// create websocket server over tls
+if (serverConfig.websocket_ssl) {
+  const credentials = loadTlsInfo(
+    `${__dirname}/../ecc_cert/ecc.key`,
+    `${__dirname}/../ecc_cert/ecc.crt`,
+  );
+  serversRunning.websocket_ssl_server = https.createServer(credentials);
+  const io = socketIO(serversRunning.websocket_ssl_server);
+  webSocketService(io);
+}
+startServer(serversRunning);
